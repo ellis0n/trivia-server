de30b1fbe80e06a155b539d08639ea67f72aeb73
glen may <ellisonglenmay@gmail.com>
2023-02-03 12:13:25 -0330

    schema fix, prompt schema created but not used

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -5,29 +5,36 @@ const configuration = new Configuration({
 })
 const openai = new OpenAIApi(configuration)
 const Question = require('../model/Question')
+const Prompt = require('../model/Prompt')
 
 const query = async (req, res) => {
     let topic = req.body.topic
     console.log(topic)
 
-    // Break this out into a collection of prompts to be referenced by the user
-    const prompt = `Generate a concise but challenging trivia question about "${topic}". Provide the answer along with three incorrect but plausible answers in the following format: {"q": value, "a": value, "w1": value, "w2": value, "w3": value}. `
+    // Break this out into a collection of prompts to be referenced by key name
+    const prompt = {
+        name: 'prompt-1',
+        prompt: `Generate a concise but challenging trivia question about "${topic}". Provide the answer along with three incorrect but plausible answers in the following format: {"q": value, "a": value, "w1": value, "w2": value, "w3": value}. `,
+    }
 
     try {
         const queryAI = await openai.createCompletion({
             model: 'text-davinci-003',
-            prompt: prompt,
+            prompt: prompt.prompt,
             max_tokens: 75,
             //   temperature: 0,
         })
         const answer = queryAI.data.choices[0].text
         console.log(answer)
         let scrubbedStr = answer.replace(/\n/g, '')
+        // console.log(scrubbedStr)
 
-        // await Question.create({
-        //     prompt: prompt,
-        //     answer: answer,
-        // })
+        const saveQuestion = await Question.create({
+            prompt: prompt.name,
+            topic: topic,
+            answer: answer,
+        })
+
         res.status(200).json({
             message: 'Query Successful',
             response: scrubbedStr,
--- model/Question.js
+++ model/Question.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const questionSchema = new Schema({
     prompt: { type: 'String', required: true },
+    topic: { type: 'String', required: true },
     answer: { type: 'String', required: true },
 })
 module.exports = mongoose.model('Question', questionSchema)
